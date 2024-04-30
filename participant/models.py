65666a4af0a2579c790f922d1cf4cc46764baab4
Jahongir <jahongirq702@gmail.com>
2024-05-01 03:40:28 +0500

    dews

---
+++ participant/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Participant(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
+
+# Create your models here.
